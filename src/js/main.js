b991a6a4ef507030913d1bf7696bb0a42fb85d15
wideirp <51214984+wideirp@users.noreply.github.com>
2024-08-19 12:09:32 -0500

    Change share btn color when active

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,9 @@ shareBtn.addEventListener("click", (event) => {
   const currentState = menu.dataset.state;
   if (currentState === "opened") {
     menu.dataset.state = "closed";
+    shareBtn.dataset.state = "";
   } else {
     menu.dataset.state = "opened";
+    shareBtn.dataset.state = "active";
   }
 });
